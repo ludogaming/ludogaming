4311aa77b641618f5a7061562206810e04018bc1
ludogaming <provost-labbe.ludovic@carrefour.cegepvicto.ca>
2019-11-21 09:50:27 -0500

    modif

--- designweb/Examen2 JavaScript Photociti/js/examen.js
+++ designweb/Examen2 JavaScript Photociti/js/examen.js
@@ -1,11 +1,13 @@
 
+var keycode = (event.keyCode ? event.keyCode : event.which);
+if (keycode == '13') {
 
 $('#sumbit').click(function (event) {
     longueur();
     treslong();
 });
 // je vais faire la methode tres longue il y a surment une methode plus simple mais ses la seul facon de faire que j'ai trouvé
-function treslong() {
+/*function treslong() {
     var mot=$('#mots').val().toUpperCase();
   var l1=mot[0];
     var l2=mot[1];
@@ -25,21 +27,189 @@ function treslong() {
         $('#modif').addClass("col-4");
         $('#modif').addClass("offset-4");
         if (l1==="A"){
-            $('#modif').prepend('<img src="Letters/A/A1.jpg" />')
+            $('#modif').prepend('<img src="Letters/A/A1.jpg" />');
+            if (l2==="A"){
+                $('#modif').append('<img src="Letters/A/A1.jpg" height="250px" width="250px" />');
+            }
+            else if (l2==="B"){
+                $('#modif').prepend('<img src="Letters/B/B1.jpg" />');
+            }
+            else if (l2==="C"){
+                $('#modif').prepend('<img src="Letters/C/C1.jpg" />');
+            }
+            else if (l2==="D"){
+                $('#modif').prepend('<img src="Letters/D/D1.jpg" />');
+            }
+            else if (l2==="E"){
+                $('#modif').prepend('<img src="Letters/E/E1.jpg" />');
+            }
+            else if (l2==="F"){
+                $('#modif').prepend('<img src="Letters/F/F1.jpg" />');
+            }
+            else if (l2==="G"){
+                $('#modif').prepend('<img src="Letters/G/G1.jpg" />');
+            }
+            else if (l2==="H"){
+                $('#modif').prepend('<img src="Letters/H/H1.jpg" />');
+            }
+            else if (l2==="I"){
+                $('#modif').prepend('<img src="Letters/I/I1.jpg" />');
+            }
+            else if (l2==="J"){
+                $('#modif').prepend('<img src="Letters/J/J1.jpg" />');
+            }
+            else if (l2==="K"){
+                $('#modif').prepend('<img src="Letters/K/K1.jpg" />');
+            }
+            else if (l2==="L"){
+                $('#modif').prepend('<img src="Letters/L/L1.jpg" />');
+            }
+            else if (l2==="M"){
+                $('#modif').prepend('<img src="Letters/M/M1.jpg" />');
+            }
+            else if (l2==="N"){
+                $('#modif').prepend('<img src="Letters/N/N1.jpg" />');
+            }
+            else if (l2==="O"){
+                $('#modif').prepend('<img src="Letters/O/O1.jpg" />');
+            }
+            else if (l2==="P"){
+                $('#modif').prepend('<img src="Letters/P/P1.jpg" />');
+            }
+            else if (l2==="Q"){
+                $('#modif').prepend('<img src="Letters/Q/Q1.jpg" />');
+            }
+            else if (l2==="R"){
+                $('#modif').prepend('<img src="Letters/R/R1.jpg" />');
+            }
+            else if (l2==="S"){
+                $('#modif').prepend('<img src="Letters/S/S1.jpg" />');
+            }
+            else if (l2==="T"){
+                $('#modif').prepend('<img src="Letters/T/T1.jpg" />');
+            }
+            else if (l2==="U"){
+                $('#modif').prepend('<img src="Letters/U/U1.jpg" />');
+            }
+            else if (l2==="V"){
+                $('#modif').prepend('<img src="Letters/V/V1.jpg" />');
+            }
+            else if (l2==="W"){
+                $('#modif').prepend('<img src="Letters/W/W1.jpg" />');
+            }
+            else if (l2==="X"){
+                $('#modif').prepend('<img src="Letters/X/X1.jpg" />');
+            }
+            else if (l2==="Y"){
+                $('#modif').prepend('<img src="Letters/Y/Y1.jpg" />');
+            }
+            else if (l2==="Z"){
+                $('#modif').prepend('<img src="Letters/Z/Z1.jpg" />');
+            }
+            else if (l2==="*"){
+                $('#modif').prepend('<img src="Letters/CS/CS1.jpg" />');
+            }
+        }
+        else if (l1==="B"){
+            $('#modif').prepend('<img src="Letters/B/B1.jpg" />');
+        }
+        else if (l1==="C"){
+            $('#modif').prepend('<img src="Letters/C/C1.jpg" />');
+        }
+        else if (l1==="D"){
+            $('#modif').prepend('<img src="Letters/D/D1.jpg" />');
+        }
+        else if (l1==="E"){
+            $('#modif').prepend('<img src="Letters/E/E1.jpg" />');
+        }
+        else if (l1==="F"){
+            $('#modif').prepend('<img src="Letters/F/F1.jpg" />');
+        }
+        else if (l1==="G"){
+            $('#modif').prepend('<img src="Letters/G/G1.jpg" />');
+        }
+        else if (l1==="H"){
+            $('#modif').prepend('<img src="Letters/H/H1.jpg" />');
+        }
+        else if (l1==="I"){
+            $('#modif').prepend('<img src="Letters/I/I1.jpg" />');
+        }
+        else if (l1==="J"){
+            $('#modif').prepend('<img src="Letters/J/J1.jpg" />');
+        }
+        else if (l1==="K"){
+            $('#modif').prepend('<img src="Letters/K/K1.jpg" />');
+        }
+        else if (l1==="L"){
+            $('#modif').prepend('<img src="Letters/L/L1.jpg" />');
+        }
+        else if (l1==="M"){
+            $('#modif').prepend('<img src="Letters/M/M1.jpg" />');
+        }
+        else if (l1==="N"){
+            $('#modif').prepend('<img src="Letters/N/N1.jpg" />');
+        }
+        else if (l1==="O"){
+            $('#modif').prepend('<img src="Letters/O/O1.jpg" />');
+        }
+        else if (l1==="P"){
+            $('#modif').prepend('<img src="Letters/P/P1.jpg" />');
+        }
+        else if (l1==="Q"){
+            $('#modif').prepend('<img src="Letters/Q/Q1.jpg" />');
+        }
+        else if (l1==="R"){
+            $('#modif').prepend('<img src="Letters/R/R1.jpg" />');
+        }
+        else if (l1==="S"){
+            $('#modif').prepend('<img src="Letters/S/S1.jpg" />');
+        }
+        else if (l1==="T"){
+            $('#modif').prepend('<img src="Letters/T/T1.jpg" />');
+        }
+        else if (l1==="U"){
+            $('#modif').prepend('<img src="Letters/U/U1.jpg" />');
+        }
+        else if (l1==="V"){
+            $('#modif').prepend('<img src="Letters/V/V1.jpg" />');
+        }
+        else if (l1==="W"){
+            $('#modif').prepend('<img src="Letters/W/W1.jpg" />');
+        }
+        else if (l1==="X"){
+            $('#modif').prepend('<img src="Letters/X/X1.jpg" />');
+        }
+        else if (l1==="Y"){
+            $('#modif').prepend('<img src="Letters/Y/Y1.jpg" />');
+        }
+        else if (l1==="Z"){
+            $('#modif').prepend('<img src="Letters/Z/Z1.jpg" />');
+        }
+        else if (l1==="*"){
+            $('#modif').prepend('<img src="Letters/CS/CS1.jpg" />');
         }
 
 
+
+
+
+
+
+
+
+
+
     }
-}
+}*/
 
 function longueur() {
     var longeur=$('#mots').val();
     if (longeur.length<3){
-        $('#lol').html("trop cours");
+        $('#lol2').html("<br>trop cours");
     }
     else if (longeur.length>12){
-        $('#lol').html("trop long");
+        $('#lol2').html("<br>trop long");
     }
-    else  $('#lol').html("");
+    else  $('#lol2').html("");
 
 }
