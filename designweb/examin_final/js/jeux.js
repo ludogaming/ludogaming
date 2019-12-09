1d8a52577084d54f47c6ff8c0f249efe2fd7d6fe
ludogaming <provost-labbe.ludovic@carrefour.cegepvicto.ca>
2019-12-08 20:04:38 -0500

    fini style navbar reste jqury nav

---
+++ designweb/examin_final/js/jeux.js
@@ -0,0 +1,132 @@
+
+$('#tori').hover(function () {
+    $(this).mouseenter(function () {
+
+
+   $(this).addClass("animated");
+    $(this).addClass("shake");
+    $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#papyrus').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#undyne').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#theundying').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#mettaton').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#mettatonex').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#asgore').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#asriel').hover(function () {
+    $(this).mouseenter(function () {
+
+
+        $(this).addClass("animated");
+        $(this).addClass("shake");
+        $(this).addClass("infinite");
+    });
+    $(this).mouseleave(function () {
+        $(this).removeClass("animated");
+        $(this).removeClass("shake");
+        $(this).removeClass("infinite");
+    });
+});
+$('#styminecraft').click(function () {
+   $('.navbar').attr("id","minecraft");
+   $('#imagebrand').attr("src","img/580b57fcd9996e24bc43c2f1.png");
+   $('#youtube').css("color","#11d8c3");
+   $('#switch').removeClass("btn-outline-dark");
+    $('#switch').addClass("btn-info");
+});
+$('#styundertale').click(function () {
+    $('.navbar').attr("id","undertale");
+});
+$('#styzelda').click(function () {
+   $('.navbar').attr("id","zelda");
+});
+$('#stycallofduty').click(function () {
+    $('.navbar').attr("id","callofduty");
+});
+$('#styoutube').click(function () {
+    $('.navbar').attr("id", "youtube");
+});
