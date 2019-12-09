8aa1e164e1b616c9be9c7073981eac7804afac02
ludogaming <provost-labbe.ludovic@carrefour.cegepvicto.ca>
2019-12-08 20:35:21 -0500

    fini style navbar reste jqury nav

--- designweb/examin_final/js/jeux.js
+++ designweb/examin_final/js/jeux.js
@@ -114,19 +114,49 @@ $('#asriel').hover(function () {
 $('#styminecraft').click(function () {
    $('.navbar').attr("id","minecraft");
    $('#imagebrand').attr("src","img/580b57fcd9996e24bc43c2f1.png");
-   $('#youtube').css("color","#11d8c3");
-   $('#switch').removeClass("btn-outline-dark");
-    $('#switch').addClass("btn-info");
+   $('.glouglou').html("style navbar: minecraft");
+    $('#titrenav').css("color","#c82333");
+    $('.fa-youtube').css("color","#FF0000");
+    $('#switch').removeClass("btn-light");
+    $('#switch').addClass("btn-outline-dark");
+
+
 });
 $('#styundertale').click(function () {
     $('.navbar').attr("id","undertale");
+    $('#imagebrand').attr("src","img/2316613_0.jpg");
+    $('.glouglou').html("style navbar: undertale");
+    $('#titrenav').css("color","#c82333");
+    $('.fa-youtube').css("color","#FF0000");
+    $('#switch').removeClass("btn-light");
+    $('#switch').addClass("btn-outline-dark");
+
 });
 $('#styzelda').click(function () {
    $('.navbar').attr("id","zelda");
+    $('#imagebrand').attr("src","img/The_Legend_of_Zelda_Ocarina_of_Time_Logo.png");
+    $('.glouglou').html("style navbar: zelda");
+    $('#titrenav').css("color","#c82333");
+    $('.fa-youtube').css("color","#FF0000");
+    $('#switch').removeClass("btn-light");
+    $('#switch').addClass("btn-outline-dark");
 });
 $('#stycallofduty').click(function () {
     $('.navbar').attr("id","callofduty");
+    $('#imagebrand').attr("src","img/Call_of_Duty_Infinite_Warfare_–_Game_logo.svg.png");
+    $('.glouglou').html("style navbar: call of duty");
+    $('#titrenav').css("color","#c82333");
+    $('.fa-youtube').css("color","#FF0000");
+    $('#switch').removeClass("btn-light");
+    $('#switch').addClass("btn-outline-dark");
 });
 $('#styoutube').click(function () {
     $('.navbar').attr("id", "youtube");
+    $('#imagebrand').attr("src","img/502px-Logo_of_YouTube_(2015-2017).svg.png");
+    $('.glouglou').html("style navbar: youtube");
+    $('.fa-youtube').css("color","#ffffff");
+    $('#titrenav').css("color","white");
+    $('#switch').removeClass("btn-outline-dark");
+    $('#switch').addClass("btn-light");
+
 });
